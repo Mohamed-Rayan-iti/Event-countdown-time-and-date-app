13e02b494a7d0c69e12a47bfe8bd6d92c3ffaad1
Mohamed Rayan <mohamed.rayan.iti.developer@outlook.com>
2021-01-27 16:48:16 +0200

    update

--- res/js/scriptImport.js
+++ res/js/scriptImport.js
@@ -1,8 +1,23 @@
 import * as importModule from "./scriptExport.js";
 
 document.getElementById("submit").onclick = () => {
-  importModule.eventCountDownDateTime(
-    new Date(document.getElementById("eventDateTime").value).getTime(),
-    document.getElementById("eventName").value
-  );
+  console.log(document.getElementById("eventDateTime").value);
+  if (
+    document.getElementById("eventName").value.trim() == "" ||
+    document.getElementById("eventDateTime").value.trim() == ""
+  ) {
+    document.getElementById("viewCountDownDateTime").innerHTML =
+      "please fill out all input field";
+    document.getElementById(
+      "viewCountDownDateTime"
+    ).parentElement.style.background = "red";
+  } else {
+    importModule.eventCountDownDateTime(
+      new Date(document.getElementById("eventDateTime").value).getTime(),
+      document.getElementById("eventName").value
+    );
+    document
+      .getElementById("viewCountDownDateTime")
+      .parentElement.removeAttribute("style");
+  }
 };
